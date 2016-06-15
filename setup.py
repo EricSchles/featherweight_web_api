9c282f6cee649afc5cb9151b5ed23e6c49d98345
Eric Schles <ericschles@gmail.com>
2016-06-15 10:23:38 -0400

    updating

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="featherweight_api",
+    version="0.1",
+    description="a simple tool for generating api's for functions",
+    author="Ian Ozsvald",
+    packages=["featherweight_api"]
+)
